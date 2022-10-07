db9f242edac1abfc2d0cb001cf30f05ff1561b36
Michiel De Muynck <michieldemuynck@gmail.com>
2022-10-07 18:17:13 +0200

    Add running bf code

--- www/index.js
+++ www/index.js
@@ -37,18 +37,29 @@ for (let example_name of examples_order) {
 }
 document.getElementById("hir_code").textContent = examples[default_example];
 
+// Compiling:
+
 function clickCompile() {
     let hir = document.getElementById("hir_code").value;
-
-    console.log(hir);
 
     let compiled = wasm.compile(hir);
 
     document.getElementById("compiled_sam").textContent = compiled.sam;
     document.getElementById("compiled_bf").textContent = compiled.bf;
 }
-
 document.getElementById("compile_button").onclick = clickCompile;
+
+// Running:
+
+function clickRun() {
+    let bf = document.getElementById("compiled_bf").value;
+    let input = document.getElementById("bf_input").value;
+
+    let result = wasm.parse_and_run_bf(bf, input);
+
+    document.getElementById("bf_output").textContent = result;
+}
+document.getElementById("run_button").onclick = clickRun;
 
 document.getElementById("bf_copy_button").onclick = function() {
     let textarea = document.getElementById("compiled_bf");
